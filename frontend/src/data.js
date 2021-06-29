51fac7eb1f0c767545324cc839f94827921d1ad3
TendaiChi <82814780+TendaiChi@users.noreply.github.com>
2021-06-29 15:14:44 +0200

    Share code to Github

---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'PDV Premium Package',
+            category: 'Packages',
+            image: '/images/Picture1.jpg',
+            price: 12090,
+            brand: 'TeMphoLifestyle@PontDeVal',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'A memorable experience to enjoy with your special someone. Complete with boat cruise and spa package.',
+        },
+        {
+            _id: '2',
+            name: 'PDV Deluxe Package',
+            category: 'Packages',
+            image: '/images/Picture2.jpg',
+            price: 13270,
+            brand: 'TeMphoLifestyle@PontDeVal',
+            rating: 5,
+            numReviews: 12,
+            description: 'A memorable experience to enjoy with your special someone. Complete with boat cruise, spa package, picnic and wine tasting. ',
+        },        {
+            _id: '3',
+            name: 'PDV Ultimate Package',
+            category: 'Packages',
+            image: '/images/Picture3.jpg',
+            price: 16790,
+            brand: 'TeMphoLifestyle@PontDeVal',
+            rating: 4.5,
+            numReviews: 16,
+            description: 'A memorable experience to enjoy with your special someone. Complete with catered boat cruise, half-day spa package, decorated picnic and private wine-making tour.',
+        },        {
+            _id: '4',
+            name: 'PDV Honeymoon Odyssey',
+            category: 'Packages',
+            image: '/images/Picture4.jpg',
+            price: 20500,
+            brand: 'TeMphoLifestyle@PontDeVal',
+            rating: 5,
+            numReviews: 20,
+            description: 'The complete romantic escape.  This package comes with a rose-petal decorated room, a private boat-cruise with 3-course menu, half-day spa package, decorated picnic and luxury wine-making tour.',
+        },        {
+            _id: '5',
+            name: 'Design Your Own Experience',
+            category: 'Packages',
+            image: '/images/Picture5.jpg',
+            price: 'YOU DECIDE!',
+            brand: 'TeMphoLifestyle@PontDeVal',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'Design an experience to suit your preferences and budget.',
+        },
+    ]
+}
+export default data;
